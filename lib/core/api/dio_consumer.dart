2b481101db4f00ac721d1fe3086320122ead344f
Ziad Salah <zslah2800@gmail.com>
2024-07-15 14:20:07 +0300

    dio_consumer methods has been completed exceptions

--- lib/core/api/dio_consumer.dart
+++ lib/core/api/dio_consumer.dart
@@ -1,4 +1,5 @@
 import 'package:api_catalog/core/api/api_consumer.dart';
+import 'package:api_catalog/core/errors/exceptions.dart';
 import 'package:dio/dio.dart';
 
 class DioConsumer extends ApiConsumer {
@@ -6,22 +7,49 @@ class DioConsumer extends ApiConsumer {
   DioConsumer(this.dio);
   @override
   deleteMethod(
-      String path, Object? data, Map<String, dynamic>? queryParameter) {
-    throw UnimplementedError();
+      String path, Object? data, Map<String, dynamic>? queryParameter) async {
+    try {
+      final response =
+          await dio.delete(path, data: data, queryParameters: queryParameter);
+      return response.data;
+    } on DioException catch (e) {
+      dioExceptions(e);
+    }
   }
 
   @override
-  getMethod(String path, Object? data, Map<String, dynamic>? queryParameter) {
-    throw UnimplementedError();
+  getMethod(
+      String path, Object? data, Map<String, dynamic>? queryParameter) async {
+    try {
+      final response =
+          await dio.get(path, data: data, queryParameters: queryParameter);
+      return response.data;
+    } on DioException catch (e) {
+      dioExceptions(e);
+    }
   }
 
   @override
-  patchMethod(String path, Object? data, Map<String, dynamic>? queryParameter) {
-    throw UnimplementedError();
+  patchMethod(
+      String path, Object? data, Map<String, dynamic>? queryParameter) async {
+    try {
+      final response =
+          await dio.patch(path, data: data, queryParameters: queryParameter);
+      return response.data;
+    } on DioException catch (e) {
+      dioExceptions(e);
+    }
   }
 
   @override
-  postMethod(String path, Object? data, Map<String, dynamic>? queryParameter) {
-    throw UnimplementedError();
+  postMethod(
+      String path, Object? data, Map<String, dynamic>? queryParameter) async {
+    try {
+      final response =
+          await dio.post(path, data: data, queryParameters: queryParameter);
+      return response.data;
+    } on DioException catch (e) {
+      dioExceptions(e);
+    }
   }
 }
