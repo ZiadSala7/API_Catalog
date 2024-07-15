a1d6117821acaa25e205e378e74785157d835810
Ziad Salah <zslah2800@gmail.com>
2024-07-15 15:02:33 +0300

    api_interceptors has been completed endPoints file has been created

--- lib/core/api/api_consumer.dart
+++ lib/core/api/api_consumer.dart
@@ -1,22 +1,16 @@
 abstract class ApiConsumer {
-  getMethod(
+  Future<dynamic> getMethod(
     String path,
     Object? data,
     Map<String, dynamic>? queryParameter,
   );
-  postMethod(
-    String path,
-    Object? data,
-    Map<String, dynamic>? queryParameter,
-  );
-  patchMethod(
-    String path,
-    Object? data,
-    Map<String, dynamic>? queryParameter,
-  );
-  deleteMethod(
-    String path,
-    Object? data,
-    Map<String, dynamic>? queryParameter,
-  );
+  Future<dynamic> postMethod(
+      String path, Object? data, Map<String, dynamic>? queryParameter,
+      {bool isFormData = false});
+  Future<dynamic> patchMethod(
+      String path, Object? data, Map<String, dynamic>? queryParameter,
+      {bool isFormData = false});
+  Future<dynamic> deleteMethod(
+      String path, Object? data, Map<String, dynamic>? queryParameter,
+      {bool isFormData = false});
 }
--- lib/core/api/dio_consumer.dart
+++ lib/core/api/dio_consumer.dart
@@ -1,10 +1,25 @@
 import 'package:api_catalog/core/api/api_consumer.dart';
+import 'package:api_catalog/core/api/api_interceptors.dart';
+import 'package:api_catalog/core/api/endpoints.dart';
 import 'package:api_catalog/core/errors/exceptions.dart';
 import 'package:dio/dio.dart';
 
 class DioConsumer extends ApiConsumer {
   final Dio dio;
-  DioConsumer(this.dio);
+  DioConsumer(this.dio) {
+    dio.options.baseUrl = Endpoints.baseUrl;
+    //ApiInterceptors => can override its methods
+    dio.interceptors.add(ApiInterceptors());
+    //has a ready functions to use to prints details  of: response, request, request headers and errors
+    dio.interceptors.add(LogInterceptor(
+      request: true,
+      requestHeader: true,
+      requestBody: true,
+      responseBody: true,
+      responseHeader: true,
+      error: true,
+    ));
+  }
 
   @override
   Future getMethod(
@@ -20,10 +35,12 @@ class DioConsumer extends ApiConsumer {
 
   @override
   Future postMethod(
-      String path, Object? data, Map<String, dynamic>? queryParameter) async {
+      String path, dynamic data, Map<String, dynamic>? queryParameter,
+      {bool isFormData = false}) async {
     try {
-      final response =
-          await dio.post(path, data: data, queryParameters: queryParameter);
+      final response = await dio.post(path,
+          data: isFormData ? FormData.fromMap(data) : data,
+          queryParameters: queryParameter);
       return response.data;
     } on DioException catch (e) {
       dioExceptions(e);
@@ -32,10 +49,12 @@ class DioConsumer extends ApiConsumer {
 
   @override
   Future patchMethod(
-      String path, Object? data, Map<String, dynamic>? queryParameter) async {
+      String path, dynamic data, Map<String, dynamic>? queryParameter,
+      {bool isFormData = false}) async {
     try {
-      final response =
-          await dio.patch(path, data: data, queryParameters: queryParameter);
+      final response = await dio.patch(path,
+          data: isFormData ? FormData.fromMap(data) : data,
+          queryParameters: queryParameter);
       return response.data;
     } on DioException catch (e) {
       dioExceptions(e);
@@ -44,10 +63,12 @@ class DioConsumer extends ApiConsumer {
 
   @override
   Future deleteMethod(
-      String path, Object? data, Map<String, dynamic>? queryParameter) async {
+      String path, dynamic data, Map<String, dynamic>? queryParameter,
+      {bool isFormData = false}) async {
     try {
-      final response =
-          await dio.delete(path, data: data, queryParameters: queryParameter);
+      final response = await dio.delete(path,
+          data: isFormData ? FormData.fromMap(data) : data,
+          queryParameters: queryParameter);
       return response.data;
     } on DioException catch (e) {
       dioExceptions(e);
